e0cb63cc760108ff3eff069b72934bb533b557cc
yumemi-rnose <r_nose@yumemi.co.jp>
2024-07-10 10:48:41 +0900

    feat: Reload ボタンをタップして天気予報を取得し、天気予報を画面に表示する

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/cloudy.svg
+  String get cloudy => 'assets/icons/cloudy.svg';
+
+  /// File path: assets/icons/rainy.svg
+  String get rainy => 'assets/icons/rainy.svg';
+
+  /// File path: assets/icons/sunny.svg
+  String get sunny => 'assets/icons/sunny.svg';
+
+  /// List of all assets
+  List<String> get values => [cloudy, rainy, sunny];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_training/gen/assets.gen.dart';
+import 'package:flutter_training/weather_model.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key,});
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  final WeatherModel _model = WeatherModel(YumemiWeather());
+  WeatherType _weatherType = WeatherType.non;
+
+  void updateWeatherType() {
+    setState(() {
+      _weatherType = _model.fetchCondition();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
     final textStyle = Theme.of(context).textTheme.labelLarge;
+
     return Scaffold(
         body: Center(
           child: FractionallySizedBox(
@@ -16,9 +34,9 @@ class MainScreen extends StatelessWidget {
                 const Spacer(),
                 Column(
                   children: [
-                    const AspectRatio(
+                    AspectRatio(
                       aspectRatio: 1,
-                      child: Placeholder(),
+                      child: _WeatherImage(weatherType: _weatherType),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -65,7 +83,7 @@ class MainScreen extends StatelessWidget {
                           ),
                           Expanded(
                             child: TextButton(
-                              onPressed: () { },
+                              onPressed: () => { updateWeatherType() },
                               child: Text(
                                 'Reload',
                                 style: textStyle?.copyWith(color: Colors.blue),
@@ -82,5 +100,21 @@ class MainScreen extends StatelessWidget {
           ),
         ),
     );
+  }
+}
+
+class _WeatherImage extends StatelessWidget {
+  const _WeatherImage({required this.weatherType, super.key});
+
+  final WeatherType weatherType;
+  @override
+  Widget build(BuildContext context) {
+    final imageWidget = switch (weatherType) {
+      WeatherType.non => const Placeholder(),
+      WeatherType.sunny => SvgPicture.asset(Assets.icons.sunny),
+      WeatherType.cloudy => SvgPicture.asset(Assets.icons.cloudy),
+      WeatherType.rainy => SvgPicture.asset(Assets.icons.rainy),
+    };
+    return imageWidget;
   }
 }
