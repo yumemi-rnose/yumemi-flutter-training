8ddb8ceeead2d5f4b6c3b77716f97e0ff36d6409
yumemi-rnose <r_nose@yumemi.co.jp>
2024-07-12 14:09:51 +0900

    feat: 起動画面に設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/weather_screen.dart';
+import 'package:flutter_training/green_back_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: WeatherScreen(),
+      home: GreenBackScreen(),
     );
   }
 }
