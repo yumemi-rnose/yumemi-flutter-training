1be30f0af51a679e19ceeeaab59a6c52099d33de
yumemi-rnose <r_nose@yumemi.co.jp>
2024-07-14 18:30:29 +0900

    refactor: 画面描画完了後の実装をmixinを使って実装し直し

--- lib/green_back_screen.dart
+++ lib/green_back_screen.dart
@@ -2,6 +2,22 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_training/weather_screen.dart';
 
+mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
+  @override
+  void initState() {
+    super.initState();
+    unawaited(afterLayout());
+  }
+
+  Future<void> afterLayout() async {
+    await WidgetsBinding.instance.endOfFrame.then((_) {
+      completion();
+    });
+  }
+
+  Future<void> completion();
+}
+
 class GreenBackScreen extends StatefulWidget {
   const GreenBackScreen({super.key});
 
@@ -9,17 +25,11 @@ class GreenBackScreen extends StatefulWidget {
   State<GreenBackScreen> createState() => _GreenBackScreenState();
 }
 
-class _GreenBackScreenState extends State<GreenBackScreen> {
+class _GreenBackScreenState extends State<GreenBackScreen>
+    with AfterLayoutMixin {
   @override
-  void initState() {
-    super.initState();
-    unawaited(_navigateAfterLayout());
-  }
-
-  Future<void> _navigateAfterLayout() async {
-    await WidgetsBinding.instance.endOfFrame.then((_) {
-      _navigate();
-    });
+  Future<void> completion() async {
+    await _navigate();
   }
 
   Future<void> _navigate() async {
